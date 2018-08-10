ce3c0a539252a5c4e40d458bea43947e0c011ede
luoxianzhuo <luoxianzhuo@jd.com>
2018-08-10 17:20:13 +0800

    first

---
+++ src/main/java/Algorithms/MaximumProductSubarray.java
@@ -0,0 +1,49 @@
+package Algorithms;
+
+/**
+ * Created by luoxianzhuo on 2018/8/10 16:22
+ *
+ * @author luoxianzhuo
+ * @copyright Copyright 2014-2018 JD.COM All Right Reserved
+ */
+
+/**
+ * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+ * <p>
+ * Example 1:
+ * Input: [2,3,-2,4]
+ * Output: 6
+ * Explanation: [2,3] has the largest product 6.
+ * <p>
+ * Example 2:
+ * Input: [-2,0,-1]
+ * Output: 0
+ * Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ */
+public class MaximumProductSubarray {
+
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length < 1) return 0;
+        if (nums.length < 2) return nums[0];
+
+        int global = nums[0];
+        int max = nums[0], min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int a = max * nums[i];
+            int b = min * nums[i];
+
+            max = Math.max(nums[i], Math.max(a, b));
+            min = Math.min(nums[i], Math.min(a, b));
+            global = Math.max(max, global);
+        }
+
+        return global;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {2,3,-2,4};
+        MaximumProductSubarray maximumProductSubarray = new MaximumProductSubarray();
+        System.out.println(maximumProductSubarray.maxProduct(A));
+        ;
+    }
+}
